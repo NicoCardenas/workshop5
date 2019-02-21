80c29c974c9c746f7f60288ccb53bc9b57ebc4fa
Nicolas Cardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2019-02-21 00:34:14 -0500

    finish at point 5

--- Cinema/src/main/java/edu/eci/arsw/cinema/filter/impl/FilteringByAvailability.java
+++ Cinema/src/main/java/edu/eci/arsw/cinema/filter/impl/FilteringByAvailability.java
@@ -1,15 +1,30 @@
 package edu.eci.arsw.cinema.filter.impl;
 
 import edu.eci.arsw.cinema.filter.Filter;
+import edu.eci.arsw.cinema.model.Cinema;
+import edu.eci.arsw.cinema.model.CinemaFunction;
 import edu.eci.arsw.cinema.model.Movie;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FilteringByAvailability implements Filter{
 
     @Override
-    public List<Movie> filterBy(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<Movie> filterBy(Cinema cinema, String date, String filter) {
+        List<Movie> res = new ArrayList<>();        
+        for (CinemaFunction cinemaFunction : cinema.getFunctions()) {
+            int count = 0;
+            for (List<Boolean> seat : cinemaFunction.getSeats()) {
+                for (Boolean boolean1 : seat) {
+                    if (boolean1)
+                        count++;
+                }
+            }
+            if (count <= Integer.parseInt(filter))
+                res.add(cinemaFunction.getMovie());
+        }
+        
+        return res;
     }
-    
-    
+
 }
